8dbf8892ac6b9d8e4924e9e9684b600b02afebfb
Martin T. H. Sandsmark <martin.sandsmark@kde.org>
2021-08-12 19:30:55 +0200

    init

---
+++ Makefile
@@ -0,0 +1,19 @@
+CCFILES=$(wildcard *.cpp)
+CXXFLAGS += -g -fPIC -std=c++2a -O1 -Wall -Wextra -pedantic
+OBJECTS=$(patsubst %.cpp, %.o, $(CCFILES))
+LDFLAGS=-lsystemd -g
+
+CXXFLAGS += -fsanitize=undefined -fsanitize=address
+LDFLAGS += -fsanitize=undefined -fsanitize=address
+
+journal-watch: $(OBJECTS)
+	$(CXX) -o $@ $^ $(LDFLAGS)
+
+%.o: %.cpp
+	$(CXX) -MMD -MP $(CXXFLAGS) -o $@ -c $<
+
+DEPS=$(OBJECTS:.o=.d)
+-include $(DEPS)
+
+clean:
+	rm -f journal-watch $(OBJECTS) $(DEPS)
---
+++ journal-watch.cpp
@@ -0,0 +1,242 @@
+#include <errno.h>
+#include <limits.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/epoll.h>
+#include <sys/types.h>
+#include <pwd.h>
+
+#include <ctime>
+#include <string>
+#include <iostream>
+#include <iomanip>
+
+#include <systemd/sd-journal.h>
+
+static std::string getUsername(const std::string &uidString)
+{
+    long uid = -1;
+    try {
+        uid = stol(uidString);
+    } catch (const std::exception &) {
+        // conversion failed
+        return uidString;
+    }
+    if (uid < 0) {
+        return uidString;
+    }
+
+    // fuck the _r, we don't need it: no threads and very short lived
+    passwd *pw = getpwuid(uid);
+    if (!pw) {
+        return uidString;
+    }
+    if (strlen(pw->pw_name) == 0) {
+        return uidString;
+    }
+    return pw->pw_name;
+}
+
+static std::string fetchField(sd_journal *journal, const std::string &field)
+{
+    char *message = nullptr;
+    size_t messageLength = 0ULL;
+    int ret = sd_journal_get_data(journal, field.c_str(), (const void **) &message, &messageLength);
+
+    if (ret < 0) {
+        perror(("Failed to fetch field " + field).c_str());
+        return "";
+    }
+
+    // + 1 since the message is returned as FIELD=whatwewant
+    const size_t fieldLength = field.size() + 1;
+    const int textLength = messageLength - fieldLength;
+
+    return std::string(message + fieldLength, textLength);;
+}
+
+static int print_journal_message(sd_journal *j)
+{
+    uint64_t usec;
+    int ret = sd_journal_get_realtime_usec(j, &usec);
+    if (ret < 0) {
+        return ret;
+    }
+
+    bool highPriority = false;
+    try {
+        int priority = std::stoi(fetchField(j, "PRIORITY"));
+        if (priority < 6) {
+            highPriority = true;
+        }
+    } catch (const std::exception &) {}
+
+    time_t sec = usec / 1000000;
+    std::tm tm;
+    localtime_r(&sec, &tm);
+    std::cout
+        << "\033[02;37m"
+        << std::put_time(&tm, "%H:%M:%S %b %d ")
+        << fetchField(j, "_HOSTNAME") << ":"
+        << getUsername(fetchField(j, "_UID")) << " "
+        << fetchField(j, "_COMM") << "["
+        << fetchField(j, "_PID") << "]: "
+        << (highPriority ? "\033[01;37m" : "\033[0m")
+        << fetchField(j, "MESSAGE")
+        << "\033[0m"
+        << std::endl
+    ;
+
+    return 0;
+}
+
+int run(sd_journal * * const journal)
+{
+    int r = sd_journal_seek_tail(*journal);
+
+    if (r < 0) {
+        perror("Failed to seek to the end of system journal");
+        return errno;
+    }
+
+    /* Tail -f displays last 10 messages */
+    for (int i = 0; i < 10; i++) {
+        r = sd_journal_previous(*journal);
+
+        if (r < 0) {
+            perror("Failed to move backwards in journal");
+            return errno;
+        }
+    }
+
+    for (int i = 0; i < 10; i++) {
+        print_journal_message(*journal);
+        sd_journal_next(*journal);
+    }
+
+
+    int fd = sd_journal_get_fd(*journal);
+    if (fd < 0) {
+        perror("Failed to obtain system journal file descriptor");
+        return errno;
+    }
+
+    int epoll_fd = epoll_create1(EPOLL_CLOEXEC);
+    if (epoll_fd < 0) {
+        perror("Failed to create an epoll instance");
+        return errno;
+    }
+
+    struct epoll_event ev = {};
+    ev.events = sd_journal_get_events(*journal);
+    ev.data.fd = fd;
+
+    r = epoll_ctl(epoll_fd, EPOLL_CTL_ADD, fd, &ev);
+
+    if (r < 0) {
+        perror("Failed to add system journal file descriptor to epoll instance");
+        return errno;
+    }
+
+    uint64_t timeout = -1;
+    sd_journal_get_timeout(*journal, &timeout);
+
+    while (true) {
+        struct epoll_event e = {};
+        int events = epoll_wait(epoll_fd, &e, 1, timeout);
+
+        if (events < 0) {
+            perror("epoll_wait() failed");
+            return errno;
+        }
+
+        if (events == 0) {
+            continue;
+        }
+
+        int type = sd_journal_process(*journal);
+
+        if (type < 0) {
+            perror("Failed to process system journal event");
+            return errno;
+        }
+
+        if (type == SD_JOURNAL_NOP) {
+            continue;
+        } else if (type == SD_JOURNAL_APPEND) {
+            while (sd_journal_next(*journal)) {
+                print_journal_message(*journal);
+            }
+        } else {
+            // Invalidated or something, so reopen
+            puts("Log object invalidated, re-opening");
+
+            epoll_ctl(epoll_fd, EPOLL_CTL_DEL, fd, NULL);
+
+            // Get timestamp of previous
+            sd_journal_previous(*journal);
+
+            uint64_t usec;
+            sd_id128_t boot_id;
+            int ret = sd_journal_get_monotonic_usec(*journal, &usec, &boot_id);
+
+            bool seek = true;
+            if (ret < 0) {
+                perror("Failed to obtain monotonic timestap for current journal entry");
+                seek = false;
+            }
+
+            sd_journal_close(*journal);
+
+            ret = sd_journal_open(journal, SD_JOURNAL_SYSTEM | SD_JOURNAL_CURRENT_USER | SD_JOURNAL_LOCAL_ONLY);
+            if (ret < 0) {
+                perror("Failed to open system journal");
+                exit(EXIT_FAILURE);
+            }
+
+            if (seek) {
+                int ret = sd_journal_seek_monotonic_usec(*journal, boot_id, usec);
+
+                if (ret < 0) {
+                    perror("Failed to seek to last seen entry");
+                    sd_journal_seek_tail(*journal);
+                }
+            }
+
+            fd = sd_journal_get_fd(*journal);
+            if (fd < 0) {
+                perror("Failed to obtain system journal file descriptor");
+                return errno;
+            }
+
+            ret = epoll_ctl(epoll_fd, EPOLL_CTL_ADD, fd, &ev);
+            if (r < 0) {
+                perror("Failed to add system journal file descriptor to epoll instance");
+                return errno;
+            }
+        }
+    }
+
+    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+    (void)argc;
+    (void)argv;
+
+    sd_journal *journal;
+    int ret = sd_journal_open(&journal, SD_JOURNAL_LOCAL_ONLY);
+
+    if (ret < 0) {
+        perror("Failed to open system journal");
+        return EXIT_FAILURE;
+    }
+    ret = run(&journal);
+    if (journal) {
+        sd_journal_close(journal);
+    }
+
+    return EXIT_FAILURE;
+}
